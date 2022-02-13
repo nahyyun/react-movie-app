29c815cbf6e1ce2bb34612b3ec2690cc9551d528
nahyyun <nahyuun@naver.com>
2022-02-13 12:20:45 +0900

    api 수정

--- src/api/index.js
+++ src/api/index.js
@@ -25,14 +25,18 @@ const NowPlayingMovie = async() => {
 } 
 const UpcomingMovie = async() => {
     let Data = [];
+    let totalPage=1;
     try{
-        for(let i = 1; i <4; i++){
+        for(let i = 1; i < totalPage+1; i++){
             const res = await instance.get('/movie/upcoming',{
                 params: {
                     region: 'US',
                     page: i
                 }
             })
+            if(totalPage !== res.data.total_pages){
+                totalPage=res.data.total_pages;
+            }
             Data.push(res.data.results);
         }
         return Data;
